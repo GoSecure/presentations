94a7814b70d175ef31eaabea659d41408a934716
Philippe Arteau <parteau@gosecure.ca>
2017-12-04 17:52:23 -0800

    Add link to test origin change.

--- 2016-02-24_confoo/demos/edge-xss-attribute.php
+++ 2016-02-24_confoo/demos/edge-xss-attribute.php
@@ -3,6 +3,16 @@ header("X-XSS-Protection: 1");
 $userId = $_GET['userId']; 
 ?>
 
+
 <form action="">
 User ID : <input type="text" id="userId" name="userId" value="<?php echo $userId;?>" >
 </form>
+
+
+
+<br/>
+
+
+<a href="http://evilwebsite.com:8033/edge-xss-attribute.php">Go to evil source</a>
+
+<a href="http://localhost:8033/edge-xss-attribute.php?userId=YOLO%22%20%%20autofocus=%22%22%20onfocus=%22alert(1)">Trigger XSS</a>
